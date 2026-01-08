37ab0afb5cbf89bc4f1dc9416a61d94d402fa3da
salinder singh <salindersingh0133@gmail.com>
2026-01-08 11:55:12 +0530

    Fix image source and template literals in music player
    Updated image source for 'Fakira' track and fixed template literals in the music player system.

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const tracksData = [
         { title: "Jhol", img: "photos/music/music1.jpeg", src: "photos/music/jhol.mp3" },
         { title: "Dil Ibaadat", img: "photos/music/music2.jpeg", src: "photos/music/Dil Ibaadat.mp3" },
-        { title: "Fakira", img: "photos/music/music3n.jpeg", src: "photos/music/Fakira.mp3" },
+        { title: "Fakira", img: "photos/music/music3fix.jpeg", src: "photos/music/Fakira.mp3" },
         { title: "Tu Hi Haqeeqat", img: "photos/music/music4.jpeg", src: "photos/music/Tu Hi Haqeeqat.mp3" },
         { title: "Gale Lag Ja", img: "photos/music/music5.jpeg", src: "photos/music/Gale Lag Ja.mp3" }
     ];
@@ -359,6 +359,7 @@ window.addEventListener('popstate', () => {
 window.onload = () => {
     showSection(window.location.hash);
 };
+
 
 
 
